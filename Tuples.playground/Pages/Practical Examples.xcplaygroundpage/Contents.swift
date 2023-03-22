d3a74a638a724abf4fb174cfcf01e0f3c8181afd
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-03-22 01:08:58 +0100

    Completed Understanding Swift Tuples

--- Tuples.playground/Pages/Practical Examples.xcplaygroundpage/Contents.swift
+++ Tuples.playground/Pages/Practical Examples.xcplaygroundpage/Contents.swift
@@ -21,29 +21,56 @@ let countryDictionary: [String: Int] = [
 
 code(for: "1. Parameters in Functions") {
     // convert the countryInfo array in to one that will use a tuple for a parameter
-
+    func countryInfo(_ country: (name: String, population: Int, states: Int)) {
+        print("\(country.name) has \(country.population) people in \(country.states) states.")
+    }
+    countryInfo((name: "Canada", population: 37_000_000, states: 10))
 }
 
 code(for: "2. Returning Tuples") {
     // use the CountryDictionary to create a function that will return a tuple when provided the name of the country
-
+    func countryInfo(_ name: String) -> (name: String, population: Int?) {
+        return (name: name, population: countryDictionary[name])
+    }
+    print(countryInfo("Canada"))
+    print(countryInfo("Ukraine"))
 }
 
-code(for: "3. TypeAleas") {
+code(for: "3. TypeAlias") {
     // use a typeAlias to recreate the countryInfo function
-
+    typealias countryData = (name: String, population: Int?)
+    func countryInfo(_ name: String) -> countryData {
+        return (name: name, population: countryDictionary[name])
+    }
+    print(countryInfo("Canada"))
+    let us = countryInfo("United States")
+    print(us.population ?? "unknown")
+    let uk = countryInfo("Ukraine")
+    print(uk.population ?? "unknown")
 }
 
 code(for: "4. Covert Dictionary to Array of tuples") {
     // Map the CountryDictionary into an array of tuples
-
+    let countryArray = countryDictionary.map( { (name: $0, population: $1) })
+        .sorted { $0.name < $1.name }
+    for country in countryArray {
+        print(country.name)
+    }
 }
 
 code(for: "5. Sorting with Tuples") {
     // see also https://youtu.be/4ochXtdrd70
     // Use the Wine model and the allWines static array (found in the Sources folder for the playground)
     // To sort wines using multiple criteria
-
+    let allWines = Wine.wines
+    let sortedWines = allWines.sorted { ($0.variety, $0.winery, $0.vintage) < ($1.variety, $1.winery, $1.vintage) }
+    for wine in allWines {
+        print(wine.winery, wine.variety, wine.vintage)
+    }
+    print("----------------------------------------")
+    for wine in sortedWines {
+        print(wine.winery, wine.variety, wine.vintage)
+    }
 }
 /*:
 
--- Tuples.playground/Pages/The Basics.xcplaygroundpage/Contents.swift
+++ Tuples.playground/Pages/The Basics.xcplaygroundpage/Contents.swift
@@ -6,23 +6,35 @@ Understanding the basics of Tuples
  
  A tuple is a compound data type that works very similarly to a struct, but tuples can be created and used on the fly.
 */
+// Film: https://www.youtube.com/watch?v=-7xVz-86brg&ab_channel=StewartLynch
+
 import Foundation
 
 code(for: "1. Basic Tuple") {
     // Create a Tuple that will contain a country name and the country population along with the number of states/provinces
-   
+   let country = ("Canada", 37_000_000, 10)
+    print(country.0)
+    print(country.1)
+    print(country.2)
 }
 
 code(for: "2. Decomposing Values") {
     // Create a Tuple that will contain a country name and the country population along with the number of states/provinces
     // And decompose it to provide named values
-    
+    let country = ("Canada", 37_000_000, 10)
+    let (name, population, states) = country
+    print(name)
+    print(population)
+    print(states)
 }
 
 code(for: "3. Rewrite the Basic Tuple using named values") {
     // Create a Tuple that will contain a country name and the country population along with the number of states/provinces
     // But use named values when creating the tuple
-
+    let country = (name: "Canada", population: 37_000_000, states: 10)
+    print(country.name)
+    print(country.population)
+    print(country.states)
 }
 
 /*:
